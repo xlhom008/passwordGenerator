c94ab1fed82c6303576438219f745ab809c25190
xlhom008 <lhota.matyas@gmail.com>
2021-01-15 10:06:08 +0100

    function for the 2nd part of password

--- passwordGenerator.js
+++ passwordGenerator.js
@@ -1,17 +1,18 @@
 const Password = {
   part1 () {
     let password;
-    const nums = [...Array(5).keys()];
+    const nums = [];
+    for (let i in [...Array(5)]) {
+      nums.push(Math.floor(Math.random() * 10))
+    };
     const randNum1 = Math.floor(Math.random() * 5);
     const randNum2 = Math.floor(Math.random() * 5);
     const maxRand = Math.max(randNum1, randNum2);
     const minRand = Math.min(randNum1, randNum2);
-    console.log(minRand, maxRand);
     const order = Math.floor(Math.random() * 5);
     const a = nums.slice(0, minRand);
     const b = nums.slice(minRand, maxRand);
     const c = nums.slice(maxRand, nums.length);
-    console.log(order)
     switch (order) {
       case 0:
         password = c.concat(b).concat(a);
@@ -28,6 +29,18 @@ const Password = {
       case 4:
         password = c.concat(a).concat(b);
         break;  
-    }
+    };
+    return password.join('');
+  },
+  
+  part2 (password) {
+    let password2 = [];
+    const letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+    for (let i in [...Array(5)]) {
+      const randIn = Math.floor(Math.random() * letters.length);
+      const randChar = letters[randIn];
+      password2.push(randChar);
+    };
+    return password + password2.join('')
   }
 }
